cd4255cfdcba05413d2985bbf7afc7d77cefe2b3
Quinn Slack <qslack@qslack.com>
2019-05-28 02:40:12 -0700

    wip

--- extensions/enterprise/check-search/src/extension.ts
+++ extensions/enterprise/check-search/src/extension.ts
@@ -3,6 +3,6 @@ import { registerImportStar } from './importStar'
 import { registerNoInlineProps } from './noInlineProps'
 
 export function activate(ctx: sourcegraph.ExtensionContext): void {
-    // ctx.subscriptions.add(registerImportStar())
+    ctx.subscriptions.add(registerImportStar())
     ctx.subscriptions.add(registerNoInlineProps())
 }
