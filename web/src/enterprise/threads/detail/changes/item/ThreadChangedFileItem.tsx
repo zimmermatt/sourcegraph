08c4eac1d76d41b080634d458ab58e3c4f0215b0
Quinn Slack <qslack@qslack.com>
2019-05-28 23:18:55 -0700

    wip

--- web/src/enterprise/threads/detail/changes/item/ThreadChangedFileItem.tsx
+++ web/src/enterprise/threads/detail/changes/item/ThreadChangedFileItem.tsx
@@ -1,6 +1,9 @@
+import FileIcon from 'mdi-react/FileIcon'
 import React from 'react'
 import { Markdown } from '../../../../../../../shared/src/components/Markdown'
+import { displayRepoName } from '../../../../../../../shared/src/components/RepoFileLink'
 import { renderMarkdown } from '../../../../../../../shared/src/util/markdown'
+import { parseRepoURI } from '../../../../../../../shared/src/util/url'
 import { FileDiff } from '../computeDiff'
 
 interface Props {
@@ -18,16 +21,23 @@ export const ThreadChangedFileItem: React.FunctionComponent<Props> = ({
     className = '',
     headerClassName = '',
     headerStyle,
-}) => (
-    <div className={`card border ${className}`}>
-        <header className={`card-header d-flex align-items-start ${headerClassName}`} style={headerStyle}>
-            <div className="flex-1">
-                <h3 className="mb-0 h6">{diff.newPath || diff.oldPath!}</h3>
-            </div>
-        </header>
-        <Markdown
-            dangerousInnerHTML={renderMarkdown('```diff\n' + diff.hunks.map(h => h.body).join('\n') + '\n```')}
-            className="overflow-auto"
-        />
-    </div>
-)
+}) => {
+    const parsed = parseRepoURI(diff.newPath || diff.oldPath!)
+    return (
+        <div className={`card border ${className}`}>
+            <header className={`card-header d-flex align-items-start ${headerClassName}`} style={headerStyle}>
+                <div className="flex-1 d-flex align-items-center">
+                    <FileIcon className="icon-inline mr-2" />
+                    <h3 className="mb-0 h6">
+                        <span className="font-weight-normal">{displayRepoName(parsed.repoName)}</span> ›{' '}
+                        {parsed.filePath}
+                    </h3>
+                </div>
+            </header>
+            <Markdown
+                dangerousInnerHTML={renderMarkdown('```diff\n' + diff.hunks.map(h => h.body).join('\n') + '\n```')}
+                className="overflow-auto"
+            />
+        </div>
+    )
+}
